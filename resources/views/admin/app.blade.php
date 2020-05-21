e271e9c90f3e9e05e17fdcf351674c756a8c3d34
Md. Abdul Baten <batenduae@gmail.com>
2020-05-21 14:36:36 +0600

    mastering
    header, sidebar, app layout and dashboard view setup

---
+++ resources/views/admin/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>@yield('title') - {{config('app.name')}}</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="{{asset('/backend/css/main.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/backend/css/fontawesome-5.7.0.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/backend/css/font-awesome/4.7.0/css/font-awesome.min.css')}}">
+</head>
+<body class="app sidebar-mini">
+    @include('admin.partials.header')
+    @include('admin.partials.sidebar')
+    <main class="app-content" id="app">
+        @yield('page-content')
+    </main>
+
+    <script type="text/javascript" src="{{asset('/backend/js/jquery-3.3.1.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('/backend/js/popper.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('/backend/js/bootstrap.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('/backend/js/main.js')}}"></script>
+    <script type="text/javascript" src="{{asset('/backend/js/plugins/pace.min.js')}}"></script>
+    @stack('scripts')
+</body>
---
+++ routes/admin.php
@@ -0,0 +1,4 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+Route::view('/admin','admin.dashboard.index');
