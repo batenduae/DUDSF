eb09548c2ac53fbafc1bd0c8b2cc85078313ff2d
Md. Abdul Baten <batenduae@gmail.com>
2020-05-21 21:12:24 +0600

    SettingsTableSeeder Update
    SettingsTableSeeder Update

--- database/seeds/SettingsTableSeeder.php
+++ database/seeds/SettingsTableSeeder.php
@@ -1,110 +1,119 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use App\Models\Setting;
+use Illuminate\Database\Seeder;
 
 class SettingsTableSeeder extends Seeder
 {
+    /**
+     * @var array
+     */
     protected $settings = [
         [
-            'key' => 'site_name',
-            'value' => 'E-Commerce Application'
+            'key'                       =>  'site_name',
+            'value'                     =>  'DUDSF Web Application',
         ],
         [
-            'key' => 'site_title',
-            'value' => 'E-commerce',
+            'key'                       =>  'site_title',
+            'value'                     =>  'DUDSF',
         ],
         [
-            'key' => 'default_email_address',
-            'value' => 'admin@admin.com'
+            'key'                       =>  'default_email_address',
+            'value'                     =>  'dudsf.org@gmail.com',
         ],
         [
-            'key' => 'currency_code',
-            'value' => 'USD'
+            'key'                       =>  'currency_code',
+            'value'                     =>  'BDT',
         ],
         [
-            'key' => 'currency_symbol',
-            'value' => '$'
+            'key'                       =>  'currency_symbol',
+            'value'                     =>  '‎৳',
         ],
         [
-            'key' => 'site_logo',
-            'value' => ''
+            'key'                       =>  'site_logo',
+            'value'                     =>  '',
         ],
         [
-            'key' => 'site_favicon',
-            'value' => ''
+            'key'                       =>  'site_favicon',
+            'value'                     =>  '',
         ],
         [
-            'key' => 'footer_copyright_text',
-            'value' => 'All Rights Reserved Shopper'
+            'key'                       =>  'footer_copyright_text',
+            'value'                     =>  '',
         ],
         [
-            'key' => 'seo_meta_title',
-            'value' => ''
+            'key'                       =>  'seo_meta_title',
+            'value'                     =>  '',
         ],
         [
-            'key' => 'seo_meta_description',
-            'value' => ''
+            'key'                       =>  'seo_meta_description',
+            'value'                     =>  '',
         ],
         [
-            'key' => 'social_facebook',
-            'value' => 'https://www.Facebook.com'
+            'key'                       =>  'social_facebook',
+            'value'                     =>  '',
         ],
         [
-            'key' => 'social_twitter',
-            'value' => 'https://www.Twitter.com'
+            'key'                       =>  'social_twitter',
+            'value'                     =>  '',
         ],
         [
-            'key' => 'social_instagram',
-            'value' => 'https://www.Instagram.com'
+            'key'                       =>  'social_instagram',
+            'value'                     =>  '',
         ],
         [
-            'key' => 'social_linkedin',
-            'value' => 'https://www.LinkedIn.com'
+            'key'                       =>  'social_linkedin',
+            'value'                     =>  '',
         ],
         [
-            'key' => 'google_analytics',
-            'value' => ''
+            'key'                       =>  'google_analytics',
+            'value'                     =>  '',
         ],
         [
-            'key' => 'facebook_pixels',
-            'value' => ''
+            'key'                       =>  'facebook_pixels',
+            'value'                     =>  '',
         ],
         [
-            'key' => 'stripe_payment_method',
-            'value' => 'Disabled'
+            'key'                       =>  'stripe_payment_method',
+            'value'                     =>  '',
         ],
         [
-            'key' => 'stripe_key',
-            'value' => ''
+            'key'                       =>  'stripe_key',
+            'value'                     =>  '',
         ],
         [
-            'key' => 'stripe_secret_key',
-            'value' => ''
+            'key'                       =>  'stripe_secret_key',
+            'value'                     =>  '',
         ],
         [
-            'key' => 'paypal_payment_method',
-            'value' => 'Disabled'
+            'key'                       =>  'paypal_payment_method',
+            'value'                     =>  '',
         ],
         [
-            'key' => 'paypal_client_id',
-            'value' => ''
+            'key'                       =>  'paypal_client_id',
+            'value'                     =>  '',
         ],
         [
-            'key' => 'paypal_secret_id',
-            'value' => ''
+            'key'                       =>  'paypal_secret_id',
+            'value'                     =>  '',
         ],
     ];
 
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
     public function run()
     {
-        foreach ($this->settings as $index => $setting) {
+        foreach ($this->settings as $index => $setting)
+        {
             $result = Setting::create($setting);
             if (!$result) {
                 $this->command->info("Insert failed at record $index.");
                 return;
             }
         }
-//        $this->command->info('Inserted ' . count($this->settings) . ' records');
+        $this->command->info('Inserted '.count($this->settings). ' records');
     }
 }
